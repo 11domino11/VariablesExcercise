e301a697b8bd690f89c73687eb8d836c01dfd482
Damion Russell <domino.russell@gmail.com>
2022-07-16 11:18:45 -0400

    initial commit

---
+++ VariablesExcercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Damion Russell";
+            int age = 23;
+            char firstInitial = 'D';
+            bool isHuman = true;
+            double aDouble = 20.0;
+            decimal aDecimal =  21.1m;
+
+            Console.WriteLine("Are you human? \n" + isHuman + "\n" + "State your name. \n" + name + "\n" + "State your age. \n"
+                + age + "\n" + "His first initial is " + firstInitial + " average intelligence reading is " + aDouble + " but he's reading " + aDecimal);
+
+            Console.ReadLine(); //This is just so console doesn't close after run
+
+
+
+        }
+    }
+}
